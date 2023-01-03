85a9b139a5322aaea943db6ee574c01d690c6e60
Maria <mariamar_@hotmail.gr>
2023-01-03 15:22:14 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MMBlGHGLOAbmSZa9LsaybuWTK0OP18MhfQgUkQVLjj32B9QSt5vpsbe9dA51WUNqRSDSb8GOcbDOEaRxMhfB9Xw00cHmIvuGd',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
