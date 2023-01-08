d7a7d0c2e9cf6157d9676310e39c6a309411ba6d
Maria <mariamar_@hotmail.gr>
2023-01-08 13:08:09 +0000

    removed code for sized products from webhook handler and fixed long line

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -139,15 +139,6 @@ class StripeWH_Handler:
                             quantity=item_data,
                         )
                         order_line_item.save()
-                    else:
-                        for size, quantity in item_data['items_by_size'].items():
-                            order_line_item = OrderLineItem(
-                                order=order,
-                                product=product,
-                                quantity=quantity,
-                                product_size=size,
-                            )
-                            order_line_item.save()
             except Exception as e:
                 if order:
                     order.delete()
@@ -156,7 +147,8 @@ class StripeWH_Handler:
                     status=500)
         self._send_confirmation_email(order)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+            content=f'Webhook received: {event["type"]} | \
+                 SUCCESS: Created order in webhook',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
