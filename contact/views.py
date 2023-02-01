ccc7720993de0d4283f4a77b26d15cb561aaa461
Maria <mariamar_@hotmail.gr>
2023-02-01 15:40:46 +0000

    added contact view to handle the contact form and added form on contact.html

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 # Create your views here.
 
 
 def contact(request):
     """ A view to return the contact page """
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = ContactForm()
+    return render(request, 'contact/contact.html', {'form': form})
 
-    return render(request, 'contact/contact.html')
+
+def success(request):
+    return render(request, 'contact/success.html')
