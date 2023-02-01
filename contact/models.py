249d14ba940ff2973d64bc63db18da357392788f
Maria <mariamar_@hotmail.gr>
2023-02-01 12:39:21 +0000

    fixed long line on contact models

--- contact/models.py
+++ contact/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Contact(models.Model):
     contact_name = models.CharField(max_length=200, null=False, blank=False)
     contact_email = models.EmailField(max_length=254, null=False, blank=False)
-    contact_message = models.TextField(max_length=1000, null=False, blank=False)
+    contact_message = models.TextField(
+        max_length=1000, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
