63e3840650d75ecc82727ec5ff444bff2a9ac80e
Maria <mariamar_@hotmail.gr>
2023-02-01 10:50:13 +0000

    added contact app in installed apps and hid debug variable

--- draw_with_light/settings.py
+++ draw_with_light/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ['draw-with-light.herokuapp.com', 'localhost']
 
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'contact',
 
     # Other
     'crispy_forms',
