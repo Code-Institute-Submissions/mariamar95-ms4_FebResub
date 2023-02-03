ca3c05a87a6ee4e69ae2709193a5e4cec937cbb7
Maria <mariamar_@hotmail.gr>
2023-02-03 14:35:13 +0000

    added pop-up message to inform users when they submit contact form

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import ContactForm
-# Create your views here.
+from django.contrib import messages
 
 
 def contact(request):
@@ -9,7 +9,13 @@ def contact(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('success')
+            messages.success(request, 'Your message was sent. \
+                Please allow up to 7 days for a response')
+            return render(request, 'contact/success.html')
+        else:
+            messages.error(request, 'Your message was not sent. \
+                Please make sure all fields are filled and try again.')
+            return redirect('contact')
     else:
         form = ContactForm()
     return render(request, 'contact/contact.html', {'form': form})
